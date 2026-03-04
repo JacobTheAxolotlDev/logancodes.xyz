c41889fbeac43b926a8d7281aee0e2f0fd6ee95b
Jake <233291465+JacobTheAxolotlDev@users.noreply.github.com>
2026-03-04 18:21:13 -0500

    Implement socket connection and message sending
    Add socket connection and test message emission.

--- scripts.js
+++ scripts.js
@@ -5,3 +5,11 @@ fetch('https://api.logancodes.xyz/signup', {
 })
 .then(res => res.json())
 .then(data => console.log(data));
+
+const socket = io('https://api.logancodes.xyz');
+
+socket.on('connect', () => console.log('Connected!'));
+socket.on('message', msg => console.log('Message:', msg));
+
+// send a test message
+socket.emit('message', { text: 'hello from website' });
