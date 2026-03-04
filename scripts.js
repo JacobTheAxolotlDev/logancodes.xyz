d8adb89c87ea0958d7bd4d17aae6897e94495a3c
Jake <233291465+JacobTheAxolotlDev@users.noreply.github.com>
2026-03-04 17:44:01 -0500

    Add fetch request to signup API

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-/* Place your JavaScript in this file */
+fetch('https://api.logancodes.xyz/signup', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ username: 'logan', password: '1234' })
+})
+.then(res => res.json())
+.then(data => console.log(data));
